7f6997f5ff2d7ff4935beb95cdcf7acf4cb2acb3
Bryn Stringer <kaos.nyrb@gmail.com>
2025-12-17 14:38:15 +0000

    Prefab loading

--- questgen_tools/Nouns/Stations/StationNoun.cs
+++ questgen_tools/Nouns/Stations/StationNoun.cs
@@ -2,6 +2,7 @@
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Plugins.Assets;
 using Mutagen.Bethesda.Starfield;
+using Noggog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,7 @@ namespace FrankyCLI.questgen_tools
             string StationID = Guid.NewGuid().ToString().Substring(0, 8);
             
             //Clone the Interior
-            var intcell = CloneCellById("duout02stationtestintcell");
+            var intcell = StationUtils.CloneCellById("duout02stationtestintcell");
             intcell.EditorID = "Station_int_" + StationID;
             //Set the Interior door to be linked
             PlacedObject doorreference = null;            
@@ -42,7 +43,7 @@ namespace FrankyCLI.questgen_tools
             gen_quest_main.myMod.Cells[0].SubBlocks[0].Cells.Add(intcell);
 
             //Clone the Exterior
-            var extcell = CloneCellById("duout02stationtestextcell");
+            var extcell = StationUtils.CloneCellById("duout02stationtestextcell");
             extcell.EditorID = "Station_ext_" + StationID;
 
 
@@ -86,238 +87,43 @@ namespace FrankyCLI.questgen_tools
             }
             gen_quest_main.myMod.GenericBaseForms.Add(instance);
 
-            Console.WriteLine("Completed");
+            //Now generate the dungeon....
+            GenerateDungeon(intcell);
         }
 
-        public Cell CloneCellById(string id)
+        public void GenerateDungeon(Cell cell)
         {
-            for(int i = 0; i < gen_quest_main.myMod.Cells.Count; i++)
+            //Build the list of prefabs we can use.
+            RoomPrefab roomPrefab = new RoomPrefab("du_template_prefab_room_01");
+
+            //Find starting point
+            PlacedObject StartingMarker = null;
+            foreach(var marker in cell.Persistent)
             {
-                for (int j = 0; j < gen_quest_main.myMod.Cells[i].SubBlocks.Count; j++)
+                if (marker.EditorID != null)
                 {
-                    for(int k = 0; k < gen_quest_main.myMod.Cells[i].SubBlocks[j].Cells.Count; k++)
+                    if ( marker.EditorID == "dumarker_start")
                     {
-                        if (gen_quest_main.myMod.Cells[i].SubBlocks[j].Cells[k].EditorID == id)
-                        {
-                            var refcell = gen_quest_main.myMod.Cells[i].SubBlocks[j].Cells[k].DeepCopy();
-
-                            var persistantItems = new Noggog.ExtendedList<IPlaced>();
-                            foreach(var item in refcell.Persistent)
-                            {
-                                PlacedObject poref = (PlacedObject)item;
-                                PlacedObject placedObject = new PlacedObject(gen_quest_main.myMod)
-                                {
-                                    Action = poref.Action,
-                                    AttachRef = poref.AttachRef,
-                                    Base = poref.Base,
-                                    BlueprintPartOrigin = poref.BlueprintPartOrigin,
-                                    BOLV = poref.BOLV,
-                                    Collision = poref.Collision,
-                                    Comments = poref.Comments,
-                                    Components = poref.Components,
-                                    ConstrainedDecal = poref.ConstrainedDecal,
-                                    Count = poref.Count,
-                                    CurrentZoneCell = poref.CurrentZoneCell,
-                                    DebugText = poref.DebugText,
-                                    EditorID = poref.EditorID,
-                                    Emittance = poref.Emittance,
-                                    EnableParent = poref.EnableParent,
-                                    EncounterZone = poref.EncounterZone,
-                                    ExternalEmittance = poref.ExternalEmittance,
-                                    FactionRank = poref.FactionRank,
-                                    GeometryDirtinessScale = poref.GeometryDirtinessScale,
-                                    GroupedPackIn = poref.GroupedPackIn,
-                                    HeadTrackingWeight = poref.HeadTrackingWeight,
-                                    HealthPercent = poref.HealthPercent,
-                                    IsActivationPoint = poref.IsActivationPoint,
-                                    IsIgnoredBySandbox = poref.IsIgnoredBySandbox,
-                                    IsLinkedRefTransient = poref.IsLinkedRefTransient,
-                                    Layer = poref.Layer,
-                                    LayeredMaterialSwaps = poref.LayeredMaterialSwaps,
-                                    LevelModifier = poref.LevelModifier,
-                                    LightArea = poref.LightArea,
-                                    LightBarndoorData = poref.LightBarndoorData,
-                                    LightColors = poref.LightColors,
-                                    LightFlicker = poref.LightFlicker,
-                                    LightGobo = poref.LightGobo,
-                                    Lighting = poref.Lighting,
-                                    LightLayerData = poref.LightLayerData,
-                                    LightRoundedness = poref.LightRoundedness,
-                                    LightStaticShadowMap = poref.LightStaticShadowMap,
-                                    LightVolumetricData = poref.LightVolumetricData,
-                                    LinkedReferences = poref.LinkedReferences,
-                                    LocationRefTypes = poref.LocationRefTypes,
-                                    Lock = poref.Lock,
-                                    MapMarker = poref.MapMarker,
-                                    NavigationDoorLink = poref.NavigationDoorLink,
-                                    NumTraversalFluffBytes = poref.NumTraversalFluffBytes,
-                                    OpenByDefault = poref.OpenByDefault,
-                                    Ownership = poref.Ownership,
-                                    Patrol = poref.Patrol,
-                                    Position = poref.Position,
-                                    PersistentLocation = poref.PersistentLocation,
-                                    PowerLinks = poref.PowerLinks,
-                                    Primitive = poref.Primitive,
-                                    ProjectedDecal = poref.ProjectedDecal,
-                                    ProjectedDecalReferences = poref.ProjectedDecalReferences,
-                                    Radius = poref.Radius,
-                                    RagdollBipedRotation = poref.RagdollBipedRotation,
-                                    Properties = poref.Properties,
-                                    RagdollData = poref.RagdollData,
-                                    ReferenceGroup = poref.ReferenceGroup,
-                                    StarfieldMajorRecordFlags = poref.StarfieldMajorRecordFlags,
-                                    Rotation = poref.Rotation,
-                                    Scale = poref.Scale,
-                                    ShipArrival = poref.ShipArrival,
-                                    SnapLinks = poref.SnapLinks,
-                                    SourcePackIn = poref.SourcePackIn,
-                                    TeleportDestination = poref.TeleportDestination,
-                                    TeleportName = poref.TeleportName,
-                                    Spline = poref.Spline,
-                                    TimeOfDay = poref.TimeOfDay,
-                                    Traversals = poref.Traversals,
-                                    VolumeData = poref.VolumeData,
-                                    VirtualMachineAdapter = poref.VirtualMachineAdapter,
-                                    XALG = poref.XALG,
-                                    XCZA = poref.XCZA,
-                                    XFLG = poref.XFLG,
-                                    XNSE = poref.XNSE,
-                                    XPCK = poref.XPCK
-                                };
-                                persistantItems.Add(placedObject);
-                            }
-
-                            var tempItems = new Noggog.ExtendedList<IPlaced>();
-                            foreach (var item in refcell.Temporary)
-                            {
-                                PlacedObject poref = (PlacedObject)item;
-                                PlacedObject placedObject = new PlacedObject(gen_quest_main.myMod)
-                                {
-                                    Action = poref.Action,
-                                    AttachRef = poref.AttachRef,
-                                    Base = poref.Base,
-                                    BlueprintPartOrigin = poref.BlueprintPartOrigin,
-                                    BOLV = poref.BOLV,
-                                    Collision = poref.Collision,
-                                    Comments = poref.Comments,
-                                    Components = poref.Components,
-                                    ConstrainedDecal = poref.ConstrainedDecal,
-                                    Count = poref.Count,
-                                    CurrentZoneCell = poref.CurrentZoneCell,
-                                    DebugText = poref.DebugText,
-                                    EditorID = poref.EditorID,
-                                    Emittance = poref.Emittance,
-                                    EnableParent = poref.EnableParent,
-                                    EncounterZone = poref.EncounterZone,
-                                    ExternalEmittance = poref.ExternalEmittance,
-                                    FactionRank = poref.FactionRank,
-                                    GeometryDirtinessScale = poref.GeometryDirtinessScale,
-                                    GroupedPackIn = poref.GroupedPackIn,
-                                    HeadTrackingWeight = poref.HeadTrackingWeight,
-                                    HealthPercent = poref.HealthPercent,
-                                    IsActivationPoint = poref.IsActivationPoint,
-                                    IsIgnoredBySandbox = poref.IsIgnoredBySandbox,
-                                    IsLinkedRefTransient = poref.IsLinkedRefTransient,
-                                    Layer = poref.Layer,
-                                    LayeredMaterialSwaps = poref.LayeredMaterialSwaps,
-                                    LevelModifier = poref.LevelModifier,
-                                    LightArea = poref.LightArea,
-                                    LightBarndoorData = poref.LightBarndoorData,
-                                    LightColors = poref.LightColors,
-                                    LightFlicker = poref.LightFlicker,
-                                    LightGobo = poref.LightGobo,
-                                    Lighting = poref.Lighting,
-                                    LightLayerData = poref.LightLayerData,
-                                    LightRoundedness = poref.LightRoundedness,
-                                    LightStaticShadowMap = poref.LightStaticShadowMap,
-                                    LightVolumetricData = poref.LightVolumetricData,
-                                    LinkedReferences = poref.LinkedReferences,
-                                    LocationRefTypes = poref.LocationRefTypes,
-                                    Lock = poref.Lock,
-                                    MapMarker = poref.MapMarker,
-                                    NavigationDoorLink = poref.NavigationDoorLink,
-                                    NumTraversalFluffBytes = poref.NumTraversalFluffBytes,
-                                    OpenByDefault = poref.OpenByDefault,
-                                    Ownership = poref.Ownership,
-                                    Patrol = poref.Patrol,
-                                    Position = poref.Position,
-                                    PersistentLocation = poref.PersistentLocation,
-                                    PowerLinks = poref.PowerLinks,
-                                    Primitive = poref.Primitive,
-                                    ProjectedDecal = poref.ProjectedDecal,
-                                    ProjectedDecalReferences = poref.ProjectedDecalReferences,
-                                    Radius = poref.Radius,
-                                    RagdollBipedRotation = poref.RagdollBipedRotation,
-                                    Properties = poref.Properties,
-                                    RagdollData = poref.RagdollData,
-                                    ReferenceGroup = poref.ReferenceGroup,
-                                    Rotation = poref.Rotation,
-                                    Scale = poref.Scale,
-                                    ShipArrival = poref.ShipArrival,
-                                    SnapLinks = poref.SnapLinks,
-                                    SourcePackIn = poref.SourcePackIn,
-                                    TeleportDestination = poref.TeleportDestination,
-                                    TeleportName = poref.TeleportName,
-                                    Spline = poref.Spline,
-                                    TimeOfDay = poref.TimeOfDay,
-                                    Traversals = poref.Traversals,
-                                    VolumeData = poref.VolumeData,
-                                    VirtualMachineAdapter = poref.VirtualMachineAdapter,
-                                    XALG = poref.XALG,
-                                    XCZA = poref.XCZA,
-                                    XFLG = poref.XFLG,
-                                    XNSE = poref.XNSE,
-                                    XPCK = poref.XPCK
-                                };
-                                tempItems.Add(placedObject);
-                            }
-
-                            Cell cell = new Cell(gen_quest_main.myMod)
-                            {
-                                AcousticSpace = refcell.AcousticSpace,
-                                CellSkyRegion = refcell.CellSkyRegion,
-                                Components = refcell.Components,
-                                EditorID = id,
-                                EncounterLocation = refcell.EncounterLocation,
-                                EnvironmentMap = refcell.EnvironmentMap,
-                                Flags = refcell.Flags,
-                                GlobalDirtLayerMaterial = refcell.GlobalDirtLayerMaterial,
-                                Grid = refcell.Grid,
-                                ImageSpace = refcell.ImageSpace,
-                                Lighting = refcell.Lighting,
-                                IsLinkedRefTransient = refcell.IsLinkedRefTransient,
-                                LightingTemplate = refcell.LightingTemplate,
-                                LinkedReferences = refcell.LinkedReferences,
-                                Location = refcell.Location,
-                                MajorFlags = refcell.MajorFlags,
-                                MHDT = refcell.MHDT,
-                                Music = refcell.Music,
-                                Name = refcell.Name,
-                                Ownership = refcell.Ownership,
-                                Persistent = persistantItems,
-                                Temporary = tempItems,
-                                TimeOfDay = refcell.TimeOfDay,
-                                PersistentTimestamp = refcell.PersistentTimestamp,
-                                PersistentUnknownGroupData = refcell.PersistentUnknownGroupData,
-                                Timestamp = refcell.Timestamp,
-                                Water = refcell.Water,
-                                XCIB = refcell.XCIB,
-                                WaterEnvironmentMap = refcell.WaterEnvironmentMap,
-                                WaterHeight = refcell.WaterHeight,
-                                WaterType = refcell.WaterType,
-                                WaterVelocity = refcell.WaterVelocity,
-                                XCLAs = refcell.XCLAs,
-                                XILS = refcell.XILS,
-                                XWCN = refcell.XWCN
-                            };
-
-                            return cell;
-
-                        }
+                        StartingMarker = (PlacedObject)marker;
                     }
                 }
             }
-            return null;
+
+            //Place a prefab
+            var nextmarker = roomPrefab.Markers.Single(e => e.MarkerEditorId == "dumarker_south");            
+            P3Float pos = StartingMarker.Position + nextmarker.Position;
+            
+
+            cell.Temporary.Add(new PlacedObject(gen_quest_main.myMod)
+            {
+                Count = 1,
+                Rotation = new P3Float(),
+                Position = pos,
+                Base = roomPrefab.packin_instance.ToLink<IPlaceableObjectGetter>()
+            });
+
+            //
         }
+        
     }
 }
