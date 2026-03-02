277ada7c616e15ac827a03690369f86de6c28a99
Bryn Stringer <kaos.nyrb@gmail.com>
2026-03-02 23:23:17 +0000

    Update gen_dlgtest.bat

--- gen_dlgtest.bat
+++ gen_dlgtest.bat
@@ -1,7 +1,7 @@
 @echo off
-call .\deploy_cleanslate.bat dlgtest || goto :eof
+call .\deploy_cleanslate.bat outlaws02 || goto :eof
 dotnet build || goto :eof
-bin\Debug\net8.0\FrankyCLI.exe gen_dlgtest dlgtest || goto :eof
+bin\Debug\net8.0\FrankyCLI.exe gen_dlgtest outlaws02 || goto :eof
 pushd "C:\Program Files (x86)\Steam\steamapps\common\Starfield\Data"
-StarfieldPluginBridge.exe dlgtest.esm
+StarfieldPluginBridge.exe outlaws02.esm
 popd
